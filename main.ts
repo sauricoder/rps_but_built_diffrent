f5315ee126d40db459feb4d0c9804af60aacc0c9
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-01-31 13:11:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,93 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . . . . .
+        `)
+    OLED.clear()
+    P1 += 1
+    OLED.newLine()
+    Rounds += 1
+    updateScores()
+    basic.clearScreen()
+    if (P1 == 10) {
+        basic.clearScreen()
+        OLED.clear()
+        basic.showString("Player 1 won")
+    }
+    if (P2 == 10) {
+        basic.clearScreen()
+        OLED.clear()
+        basic.showString("Player 2 won")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    OLED.clear()
+    Rounds += 1
+    OLED.newLine()
+    Ties += 1
+    updateScores()
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # # .
+        `)
+    OLED.clear()
+    P2 += 1
+    OLED.newLine()
+    Rounds += 1
+    updateScores()
+    basic.clearScreen()
+    if (P1 == 10) {
+        basic.clearScreen()
+        OLED.clear()
+        basic.showString("Player 1 won")
+    }
+    if (P2 == 10) {
+        basic.clearScreen()
+        OLED.clear()
+        basic.showString("Player 2 won")
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
 function updateScores () {
     OLED.clear()
     OLED.writeStringNewLine("Player 1:" + P1)
     OLED.newLine()
-    OLED.writeStringNewLine("Player 1:" + P2)
+    OLED.writeStringNewLine("Player 2:" + P2)
     OLED.newLine()
-    OLED.writeStringNewLine("Player 1:" + Rounds)
+    OLED.writeStringNewLine("Rounds:" + Rounds)
     OLED.newLine()
-    OLED.writeStringNewLine("Player 1:" + Ties)
+    OLED.writeStringNewLine("Ties:" + Ties)
 }
-let P1 = 0
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Wanna play a game, kid?")
+    Rounds = 0
+    P2 = 0
+    Ties = 0
+    P1 = 0
+    basic.pause(2000)
+    updateScores()
+}
 let Ties = 0
 let P2 = 0
 let Rounds = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Wanna play a game, kid?")
-Rounds = 0
-P2 = 0
-Ties = 0
-P1 = 0
-basic.pause(2000)
-updateScores()
+let P1 = 0
+reset()
